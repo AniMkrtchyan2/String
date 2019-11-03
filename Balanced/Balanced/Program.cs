7e05be1c5ab7c4e9c371749110ac8ed1c2320265
animkr95 <animkr95@gmail.com>
2019-11-04 00:52:03 +0400

    Brackets

---
+++ Balanced/Balanced/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Balanced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string input=Console.ReadLine();
+               bool isBalanced=IsBalanced(input);
+                if (isBalanced)
+                {Console.WriteLine("Brackets are balanced");}
+                else 
+                {Console.WriteLine("wrong input");}
+               Console.ReadKey();
+    }
+
+        static bool IsBalanced(string brackets)
+            {
+           var stack= new Stack<char> ();
+            foreach(var bracket in brackets)
+                {
+                if (bracket=='(' || bracket=='[' || bracket=='{')
+                    {
+                    stack.Push(bracket);
+                    }
+                else 
+                {
+                if (stack.Count==0)
+                    {
+                        return false;
+                    }
+                char currBracket=stack.Pop();
+                    if (bracket == '}' && currBracket == '{' ||
+                        bracket == ')' && currBracket == '(' ||
+                        bracket == ']' && currBracket == '[')
+                        {
+                        continue;
+                        }
+                    else
+                        {
+                        return false;
+                      
+                         }
+                    }
+                }
+            return stack.Count()==0;}
+
+                
+        }
+     }
---
+++ String/String/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //    Console.WriteLine("Please enter text");
+            //    while (true)
+            //    {
+            //        string input = Console.ReadLine();
+            //        if (input == "exit")
+            //        {
+            //            break;
+            //        }
+            //        // string input = Console.ReadLine();
+            //        // string Output = string.Empty;
+            //        //  for (int i = 0; i < input.Length; ++i)
+            //        StringBuilder output = new StringBuilder(); //input.Substring(0, 1);
+            //        output.Append(input[0].ToString().ToUpper());
+            //        for (int i = 0; i < input.Length-1; ++i)
+            //        {
+            //            if (input[i] == ' ' && input[i + 1] != ' ')
+            //            {
+            //                // Output += input[i + 1];
+            //                output.Append(input[i+1].ToString().ToUpper());
+            //            }
+            //        }
+            //        Console.WriteLine(output);
+            //        // Console.ReadKey();
+            //        input = string.Empty;
+            //        output = null;
+            //       // Output = string.Empty;
+
+            //    }
+          
+        }
+        
+        {
+
+        }
+        
+    }
+}
